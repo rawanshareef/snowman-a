30fe0a77a5d6da929b81e87a12ef7af588b1a02f
RawanShareef <73065095+rawanshareef@users.noreply.github.com>
2021-03-10 17:00:38 +0200

    Add files via upload

---
+++ snowman.cpp
@@ -0,0 +1,7 @@
+#include <string>
+#include "snowman.hpp"
+//מימוש של הפונקצה באותו מרחב שם
+//קותבים שלד של פונקציה 
+std::string ariel::snowman(int x) {
+    return "";//becase its not work
+}
---
+++ snowman.hpp
@@ -0,0 +1,7 @@
+#include<string>
+#include<string>
+//נכליל את כל הספריה התקנית
+namespace ariel{
+    std::string snowman(int x);//פונקציה מקבלת int  ומחזירה string  
+}
+
